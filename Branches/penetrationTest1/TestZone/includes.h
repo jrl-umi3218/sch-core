76ab05d8e7b4c05dc023f0f094a4a56632cc035a
benallegue <benallegue@5988d2c4-2d00-3947-b3d2-f5ac3ac2f174>
2008-07-18 10:05:42 +0000

    Branch penetrationTest1 merged to the trunk
    git-svn-id: svn+ssh://jrlserver.muse.aist.go.jp/home/svn/Software/SCD@41 5988d2c4-2d00-3947-b3d2-f5ac3ac2f174

--- Branches/penetrationTest1/TestZone/includes.h
+++
@@ -1,9 +0,0 @@
-
-
-#include "../S_Object/S_Sphere.h"
-#include "../S_Object/S_Box.h"
-#include "../S_Object/S_Superellipsoid.h"
-#include "../STP-BV/STP_BV.h"
-#include "../CD/CD_Pair.h"
-#include "../CD/CD_Scene.h"
-#include "../S_Polyhedron/S_polyhedron.h"
