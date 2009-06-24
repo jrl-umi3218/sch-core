92d4efdf6e31bbf3d254928e0ff366c2b529a9ae
gergondet <gergondet@5988d2c4-2d00-3947-b3d2-f5ac3ac2f174>
2009-06-24 06:35:59 +0000

    You should now be able to install the library using `make install` It also generates a pkgconfig file to make integration easier
    git-svn-id: svn+ssh://jrlserver.muse.aist.go.jp/home/svn/Software/SCD@66 5988d2c4-2d00-3947-b3d2-f5ac3ac2f174

---
+++ Trunk/SCD.pc.cmake
@@ -0,0 +1,12 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${CMAKE_INSTALL_PREFIX}/bin
+libdir=${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_PROJECT_NAME}
+includedir=${CMAKE_INSTALL_PREFIX}/include/${CMAKE_PROJECT_NAME}
+datarootdir=${CMAKE_INSTALL_PREFIX}/share
+docdir=${CMAKE_INSTALL_PREFIX}/share/doc/${CMAKE_PROJECT_NAME}
+
+Name: ${PROJECT_NAME}
+Description: Library for collision detection computation
+Version: 1.0
+Libs: ${${PROJECT_NAME}_LDFLAGS} -L ${install_pkg_libdir} -lcd -lcd_penetration -lfile_parsing -ls_object -ls_polyhedron -lstp-bv
+Cflags: -I${install_pkg_include_dir}  ${${PROJECT_NAME}_CFLAGS}
