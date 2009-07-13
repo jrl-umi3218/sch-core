68fca60db3e87ca9e9741e28d21f3c3898693222
gergondet <gergondet@5988d2c4-2d00-3947-b3d2-f5ac3ac2f174>
2009-07-13 09:34:29 +0000

    Typo correction
    git-svn-id: svn+ssh://jrlserver.muse.aist.go.jp/home/svn/Software/SCD@67 5988d2c4-2d00-3947-b3d2-f5ac3ac2f174

--- Trunk/SCD.pc.cmake
+++ Trunk/SCD.pc.cmake
@@ -8,5 +8,5 @@ docdir=${CMAKE_INSTALL_PREFIX}/share/doc/${CMAKE_PROJECT_NAME}
 Name: ${PROJECT_NAME}
 Description: Library for collision detection computation
 Version: 1.0
-Libs: ${${PROJECT_NAME}_LDFLAGS} -L ${install_pkg_libdir} -lcd -lcd_penetration -lfile_parsing -ls_object -ls_polyhedron -lstp-bv
+Libs: ${${PROJECT_NAME}_LDFLAGS} -L${install_pkg_libdir} -lcd -lcd_penetration -lfile_parsing -ls_object -ls_polyhedron -lstp-bv
 Cflags: -I${install_pkg_include_dir}  ${${PROJECT_NAME}_CFLAGS}
