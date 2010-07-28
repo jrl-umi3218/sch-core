cf6e305c940aab3abb1c7d2fe434c88930627755
Francois Bleibel <fbleibel@gmail.com>
2010-07-28 15:30:52 +0900

    Corrected incorrectly generated library name
    (created library file is 'scd' compared to 'SCD' in the pkgconfig file)

--- SCD.pc.cmake
+++ SCD.pc.cmake
@@ -8,5 +8,5 @@ docdir=${CMAKE_INSTALL_PREFIX}/share/doc/${CMAKE_PROJECT_NAME}
 Name: ${PROJECT_NAME}
 Description: Library for collision detection computation
 Version: 1.0
-Libs: ${${PROJECT_NAME}_LDFLAGS} ${LIBINCL_KW}${PROJECT_NAME}${LIB_EXT}
+Libs: ${${PROJECT_NAME}_LDFLAGS} ${LIBINCL_KW}${PROJECT_LIB_NAME}${LIB_EXT}
 Cflags: -I${install_pkg_include_dir}  ${${PROJECT_NAME}_CFLAGS}
