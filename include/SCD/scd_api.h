0e5b1e33aa2e03afabdf511ff698cfcb37b3cccc
Francois Keith <francois.keith@gmail.com>
2012-04-24 23:50:06 +0200

    Change the name of the library when compiling in debug.

--- include/SCD/scd_api.h
+++ include/SCD/scd_api.h
@@ -1,6 +1,8 @@
 #ifdef WIN32
 # ifdef scd_EXPORTS
 #  define SCD_API __declspec(dllexport)
+# elif defined scdd_EXPORTS
+#  define SCD_API __declspec(dllexport)
 # else
 #  define SCD_API __declspec(dllimport)
 # endif
