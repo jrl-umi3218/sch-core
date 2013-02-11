5a7a4749a89b683612f101c95e8d36733eecf480
Francois Keith <keith@lirmm.fr>
2013-02-11 21:42:53 +0100

    Use cmake submodule.

--- doc/header.html.cmake
+++
@@ -1,7 +0,0 @@
-<HTML>
-    <HEAD>
-      <TITLE>${PROJECT_NAME} library documentation</TITLE>
-      <LINK HREF="package.css" REL="stylesheet" TYPE="text/css">
-    </HEAD>
-    <BODY>
-
--- doc/package.h.cmake
+++
@@ -1,8 +0,0 @@
-/**
-\mainpage
-
-\section ${PROJECT_NAME} Introduction
-
-${PROJECT_DESCRIPTION}
-
-*/
