ef65a8b21828f37ccfbc1c0dca8215c84ff15039
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2016-10-21 21:47:39 +0900

    Add a test for computation performances using STP_BVs

---
+++ tests/test_stp_bv_performance.cpp
@@ -0,0 +1,25 @@
+#include "shared-tests/tests/common.h"
+
+#ifdef ENABLE_SIGFPE
+  #include <fenv.h>
+#endif
+
+using namespace sch;
+
+void display() {}
+
+/*
+* main - this is the main programme.
+*/
+int main ()
+{
+  #ifdef ENABLE_SIGFPE
+  feenableexcept(FE_ALL_EXCEPT & ~FE_INEXACT);
+  #endif
+  TestMaterial universe;
+  universe.initializeUniverse();
+  universe.TestAnimation();
+  return 0;
+}
+
+
