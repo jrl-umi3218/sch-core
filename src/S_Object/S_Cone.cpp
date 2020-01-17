17e4319e0309c0c5a5cec550524d29c923d52edd
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2020-01-17 15:12:39 +0900

    Add a range exception management

--- src/S_Object/S_Cone.cpp
+++ src/S_Object/S_Cone.cpp
@@ -6,11 +6,15 @@ using namespace sch;
 S_Cone::S_Cone(const Scalar& angle, const Scalar &  height) 
 
 {
-
-  angle_=angle;
-  height_=height;
-  angleTan_=tan(angle);
-  baseRadius_=height*angleTan_;
+  if (fabs(angle) >= M_PI/2)
+  {
+    throw std::domain_error("Ivalig angle for a cone. It should be < M_PI/2");
+  }
+  
+  angle_=fabs(angle);
+  height_=fabs(angle);
+  angleTan_=tan(angle_);
+  baseRadius_=height_*angleTan_;
   
   slices_ = 100;
   stacks_ = 4; /// we don't need many stacks to display a cone
@@ -23,7 +27,6 @@ S_Cone::~S_Cone(void)
 
 Point3 S_Cone::l_Support(const Vector3& v, int& /*lastFeature*/)const
 {
-
   Scalar norm = sqrt(v[0] * v[0] + v[1] * v[1]);
   if (norm < sch::epsilon)
   {
@@ -33,14 +36,14 @@ Point3 S_Cone::l_Support(const Vector3& v, int& /*lastFeature*/)const
     } 
     else
     {
-      return Point3(0.,0.,0.);
+      return Point3(0., 0., 0.);
     }
   }
   else
   {
     if ( v[2] < - angleTan_ * norm)
     {
-      return Point3(0.,0.,0.);
+      return Point3(0., 0., 0.);
     }
     else
     {
