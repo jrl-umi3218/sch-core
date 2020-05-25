83d2e883562f10a57b42395849c005de75e979c9
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-05-25 16:47:49 +0800

    [actions] Add a conan packaging action

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ import shutil
 
 class SCHCoreConan(ConanFile):
     name = "sch-core"
-    version = "1.0.0"
+    version = "1.0.3"
     description = "Efficient implementation of GJK algorithm for proximity queries (collision detection, distance computations, penetration depths and witness points) between convex shapes."
     topics = ("robotics", "collision", "proximity", "convex")
     url = "https://github.com/jrl-umi3218/sch-core"
@@ -17,19 +17,35 @@ class SCHCoreConan(ConanFile):
     author = "Pierre Gergondet <pierre.gergondet@gmail.com>"
     license = "BSD-2-Clause"
     exports = ["LICENSE"]
-    exports_sources = ["CMakeLists.txt", "conan/CMakeLists.txt", "cmake/*", "CMakeModules/*", "doc/*", "examples/*", "include/*", "src/*", "README.md"]
-    generators = "cmake"
+    exports_sources = ["CMakeLists.txt", "conan/CMakeLists.txt", "cmake/*", "CMakeModules/*", "doc/*", "examples/*", "include/*", "src/*", "README.md", "conan/FindBoost.cmake"]
+    generators = ["cmake_find_package", "cmake_paths"]
     settings = "os", "arch", "compiler", "build_type"
 
     requires = (
         # We don't really care for a specific version of Boost and conan has >= 1.64
-        "boost/[>1.63]@conan/stable"
+        "boost/1.73.0"
     )
 
     def source(self):
         # Wrap the original CMake file to call conan_basic_setup
         shutil.move("CMakeLists.txt", "CMakeListsOriginal.txt")
         shutil.move(os.path.join("conan", "CMakeLists.txt"), "CMakeLists.txt")
+        # Make sure we find conan's Boost not system Boost
+        pattern = 'include(CMakeFindDependencyMacro)'
+        replacement = '''set(BOOST_ROOT "${{PACKAGE_PREFIX_DIR}}")
+set(Boost_NO_SYSTEM_PATHS ON)
+list(APPEND CMAKE_MODULE_PATH "${{CMAKE_CURRENT_LIST_DIR}}")
+{}'''.format(pattern)
+        tools.replace_in_file('cmake/Config.cmake.in', pattern, replacement)
+        # Install the up-to-date FindBoost.cmake
+        pattern = 'add_subdirectory(src)'
+        replacement = '''{}
+install(FILES conan/FindBoost.cmake DESTINATION lib/cmake/RBDyn)'''.format(pattern)
+        tools.replace_in_file('CMakeListsOriginal.txt', pattern, replacement)
+        # Link with Boost::Boost if consumed by conan
+        pattern = "Boost::serialization Boost::disable_autolinking"
+        replacement = "$<BUILD_INTERFACE:Boost::Boost>$<INSTALL_INTERFACE:$<IF:$<BOOL:CONAN_BOOST_ROOT>,Boost::Boost,{}>>".format(pattern.replace(' ', '$<SEMICOLON>'))
+        tools.replace_in_file('src/CMakeLists.txt', pattern, replacement)
 
     def _configure_cmake(self):
         cmake = CMake(self)
@@ -44,6 +60,10 @@ class SCHCoreConan(ConanFile):
 
     def package(self):
         cmake = self._configure_cmake()
+        pattern = "BOOL:CONAN_BOOST_ROOT"
+        replacement = "BOOL:${CONAN_BOOST_ROOT}"
+        generated = os.path.join(self.build_folder, 'CMakeFiles', 'Export', 'lib', 'cmake', self.name, '{}Targets.cmake'.format(self.name))
+        tools.replace_in_file(generated, pattern, replacement)
         cmake.install()
 
     def deploy(self):
