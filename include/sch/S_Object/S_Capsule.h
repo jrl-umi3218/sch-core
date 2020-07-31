4d51a5c42645297aa2d4b722e21006fcd5c56430
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-07-31 14:35:32 +0800

    Add some accessors to S_Capsule

--- include/sch/S_Object/S_Capsule.h
+++ include/sch/S_Object/S_Capsule.h
@@ -12,11 +12,15 @@ namespace sch
     SCH_API S_Capsule(Point3 p1, Point3 p2, Scalar radius);
     SCH_API ~S_Capsule();
 
+    SCH_API const Point3 & getP1() const;
 
+    SCH_API const Point3 & getP2() const;
+
+    SCH_API Scalar getRadius() const;
 
   protected:
     SCH_API virtual Point3 l_Support(const Vector3& v, int& lastFeature)const;
-    SCH_API virtual	S_ObjectType getType() const;
+    SCH_API virtual S_ObjectType getType() const;
 
     Point3 p1_, p2_;
 
--- src/S_Object/S_Capsule.cpp
+++ src/S_Object/S_Capsule.cpp
@@ -1,6 +1,7 @@
 #include <sch/S_Object/S_Capsule.h>
 
 using namespace sch;
+
 S_Capsule::S_Capsule(Point3 p1, Point3 p2, Scalar radius): p1_(p1),p2_(p2),radius_(radius)
 {
 }
@@ -31,4 +32,19 @@ Point3 S_Capsule::l_Support(const Vector3& v, int& /*lastFeature*/)const
 S_Object::S_ObjectType S_Capsule::getType() const
 {
   return S_Object::TCapsule;
+}
+
+const Point3 & S_Capsule::getP1() const
+{
+  return p1_;
+}
+
+const Point3 & S_Capsule::getP2() const
+{
+  return p2_;
+}
+
+Scalar S_Capsule::getRadius() const
+{
+  return radius_;
 }
