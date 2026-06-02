f6a9b7e3e56846500e601b9a971d56ec376115aa
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-02 18:28:18 +0200

    nix: add flake.lock, update mergify merge rules

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "sch-core - implementation and computation algorithms for the convex hulls";
 
   inputs.mc-rtc-nix.url = "github:mc-rtc/nixpkgs";
-  # inputs.mc-rtc-nix.url = "path:/home/arnaud/devel/mc-rtc-nix/nixpkgs";
 
   outputs =
     inputs:
